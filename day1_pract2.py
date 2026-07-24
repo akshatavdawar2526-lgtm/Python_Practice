4ab05682be61f13939d3f9ca2ec08a9f44d0b84e
akshatavdawar2526-lgtm <akshatavdawar2526@gmail.com>
2026-07-24 19:35:02 +0530

    delete old python programs

--- day1_pract2.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-add=a+b
-print("Addition=",add)
-
--- day1_pract5.py
+++
@@ -1,6 +0,0 @@
-p=float(input("Enter principal:"))
-r=float(input("Enter rate:"))
-t=float(input("EWnter time:"))
-si=(p*r*t)/100
-print("Simple Interset:",si)
-        
